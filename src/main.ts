a2597080deea027eb10e0a24f13cf99b5866b7f2
xsismadn3ss <2477carlos.artiga@gmail.com>
2025-12-10 22:53:19 -0600

    🔧 Config CORS

--- src/main.ts
+++ src/main.ts
@@ -7,6 +7,10 @@ import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
+  app.enableCors({
+    origin: ['http://localhost:3000', 'http://localhost:8081'],
+  });
+
   const isDev = app.get(ConfigService).get('NODE_ENV') === 'development';
 
   if (isDev) {
